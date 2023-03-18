38d465f9a1857375f648705f133d4d53f32f1864
MagusInverse <magusombras@gmail.com>
2023-03-17 23:24:48 -0300

    validaciones de js

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,17 @@ function guardar() {
       alert("Las claves ingresadas no coinciden");
       return false;
     }
+
+    if (nombreCompleto === "" || nombreUsuario === "" || correoElectronico === "" || claveIngreso === "" || repetirClave === "" || fechaNacimiento ==="" ){
+        alert("Favor completar todos los campos requeridos");
+        return false;
+    }
+  
+    var correoFormato = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/i;
+    if (correoFormato.test(correoElectronico)){
+        alert("Por favor ingrese una dirección de correo válida");
+        return false;
+    }
   
     var usuario = {
       nombreCompleto: nombreCompleto,
@@ -23,6 +34,10 @@ function guardar() {
   
     var usuarioJSON = JSON.stringify(usuario);
     var archivo = new File([usuarioJSON], "usuarios.txt", { type: "text/plain" });
-    var writer = new FileWriter();
-    writer.write(archivo);
+    console.log(usuarioJSON);
+    //var writer = new FileWriter();
+    //writer.write(archivo);
   }
+
+
+  
