53a62d8adda0899057623304024e11c2bd77dc80
MagusInverse <magusombras@gmail.com>
2023-03-22 23:16:06 -0300

    login agregado, index nuevo, invitado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  if (username != "" && password != "") {
+    alert("Inicio de sesión exitoso");
+    window.location.href = "../ProgramacionWebS1/index.html";
+  } else {
+    alert("Nombre de usuario o contraseña incorrectos");
+  }
+}
+
+function loginInvitado() {
+    window.location.href = "../ProgramacionWebS1/index2.html";
+  }
+
 function guardar() {
     var nombreCompleto = document.getElementById("nombre").value;
     var nombreUsuario = document.getElementById("usuario").value;
